1a7eeab9becea457211e1434fa060e050df72835
Renato Ventura <re.infotec@gmail.com>
2023-02-01 06:49:54 -0300

    [CLASS] 06. Animations

--- src/script.js
+++ src/script.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three'
-import { AxesHelper } from 'three'
+import gsap from 'gsap'
+
+console.log(gsap);
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -7,63 +9,51 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-/**
- * Objects
- */
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
 
-const group = new THREE.Group();
-scene.add(group);
-
-const cube1 = new THREE.Mesh(
-  new THREE.BoxGeometry(1,1,1),
-  new THREE.MeshBasicMaterial({ color: 0xff0000 }) 
-)
-group.add(cube1)
-
-const cube2 = new THREE.Mesh(
-  new THREE.BoxGeometry(1,1,1),
-  new THREE.MeshBasicMaterial({ color: 0x00ff00 }) 
-)
-group.add(cube2)
-cube2.position.set(1,0,0)
-
-const cube3 = new THREE.Mesh(
-  new THREE.BoxGeometry(1,1,1),
-  new THREE.MeshBasicMaterial({ color: 0x0000ff }) 
-)
-group.add(cube3)
-cube3.position.set(-1,0,0)
-
-group.rotation.y = Math.PI * .25
-
-const axesHelper = new THREE.AxesHelper();
-scene.add(axesHelper);
-
-/**
- * Sizes
- */
+// Sizes
 const sizes = {
     width: 800,
     height: 600
 }
 
-/**
- * Camera
- */
+// Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 5
-camera.position.y = 1
-camera.position.x = 1
-// camera.position.set(3, 1, 1);
+camera.position.z = 3
 scene.add(camera)
 
-/**
- * Renderer
- */
+// Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
 
-// NOTHING HERE AND BELOW IS GOING TO BE SHOWED
+// Time
+let clock = new THREE.Clock();
+
+gsap.to(mesh.position, { duration: 1, delay: 1, x:2 });
+gsap.to(mesh.position, { duration: 1, delay: 2, x:0 });
+
+// Animations
+const tick = () => {
+
+  // Clock
+  // const elapsedTime = clock.getElapsedTime();
+
+  // Update objects
+  // mesh.rotation.y = elapsedTime * Math.PI * 2;
+  // mesh.position.y = Math.sin(elapsedTime) ;
+  // mesh.position.x = Math.cos(elapsedTime) ;
+  // mesh.rotation.z = Math.tan(elapsedTime) ;
+  // camera.lookAt(mesh.position);
+
+  // Render
+  renderer.render(scene, camera)
+  window.requestAnimationFrame(tick);
+}
+
+tick();
