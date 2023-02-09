de74b33303b76b31e38ebc03896dd53db7681ce2
Renato Ventura <re.infotec@gmail.com>
2023-02-09 07:22:06 -0300

    [CLASS] 07. Cameras
    Lesson about cameras, orbit camera and stuff that makes you navigate through viewport

--- src/script.js
+++ src/script.js
@@ -1,19 +1,11 @@
 import * as THREE from 'three'
-import gsap from 'gsap'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
-console.log(gsap);
-
+/**
+ * Base
+ */
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
-
-// Scene
-const scene = new THREE.Scene()
-
-// Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
 
 // Sizes
 const sizes = {
@@ -21,10 +13,38 @@ const sizes = {
     height: 600
 }
 
+// Cursor
+const cursor = {
+    x: 0,
+    y: 0
+}
+
+window.addEventListener('mousemove', (event) =>
+{
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = - (event.clientY / sizes.height - 0.5)
+})
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const mesh = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+scene.add(mesh)
+
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+// const aspectRatio = sizes.width / sizes.height
+// const camera = new THREE.OrthographicCamera(- 1 * aspectRatio, 1 * aspectRatio, 1, - 1, 0.1, 100)
 camera.position.z = 3
 scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -32,28 +52,21 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 
-// Time
-let clock = new THREE.Clock();
+// Animate
+const clock = new THREE.Clock()
 
-gsap.to(mesh.position, { duration: 1, delay: 1, x:2 });
-gsap.to(mesh.position, { duration: 1, delay: 2, x:0 });
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
 
-// Animations
-const tick = () => {
+    // Update controls
+    controls.update()
 
-  // Clock
-  // const elapsedTime = clock.getElapsedTime();
+    // Render
+    renderer.render(scene, camera)
 
-  // Update objects
-  // mesh.rotation.y = elapsedTime * Math.PI * 2;
-  // mesh.position.y = Math.sin(elapsedTime) ;
-  // mesh.position.x = Math.cos(elapsedTime) ;
-  // mesh.rotation.z = Math.tan(elapsedTime) ;
-  // camera.lookAt(mesh.position);
-
-  // Render
-  renderer.render(scene, camera)
-  window.requestAnimationFrame(tick);
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
 }
 
-tick();
+tick()
